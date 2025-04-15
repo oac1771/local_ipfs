a54ce2062b6e763f896fa1a8f98f31c7dd738463
Omar Carey <omarcarey@Mac.hsd1.ca.comcast.net>
2025-04-15 15:50:24 -0700

    add error handling for reading and writting to state struct

--- crates/server/src/server/state.rs
+++ crates/server/src/server/state.rs
@@ -21,14 +21,18 @@ pub struct StateClient {
     tx: Sender<StateRequest>,
 }
 
+#[derive(Debug)]
 pub struct StateRequest {
     payload: StateRequestPayload,
-    sender: oneshot::Sender<Result<StateResponse, String>>,
+    sender: oneshot::Sender<Result<StateResponse, StateClientError<StateRequest>>>,
 }
+
+#[derive(Debug)]
 enum StateRequestPayload {
     AddIpfsHash { hash: String },
 }
 
+#[derive(Debug)]
 enum StateResponse {
     AddIpfsHash,
 }
@@ -38,36 +42,44 @@ impl StateClient {
         Self { tx }
     }
 
-    pub async fn add_ipfs_hash(&self, hash: String) -> Result<(), ()> {
+    pub async fn add_ipfs_hash(&self, hash: String) -> Result<(), StateClientError<StateRequest>> {
         let payload = StateRequestPayload::AddIpfsHash { hash };
-        self.send_request(payload).await;
+        self.send_request(payload).await?;
         Ok(())
     }
 
-    async fn send_request(&self, payload: StateRequestPayload) {
-        let (sender, receiver) = oneshot::channel::<Result<StateResponse, String>>();
+    async fn send_request(
+        &self,
+        payload: StateRequestPayload,
+    ) -> Result<StateResponse, StateClientError<StateRequest>> {
+        let (sender, receiver) =
+            oneshot::channel::<Result<StateResponse, StateClientError<StateRequest>>>();
         let req = StateRequest { payload, sender };
 
-        self.tx.send(req).await.unwrap();
+        self.tx
+            .send(req)
+            .await
+            .map_err(|err| StateClientError::Send { source: err })?;
 
-        let foo = Self::receive_response(receiver).await;
+        let resp = Self::receive_response(receiver).await?;
+
+        Ok(resp)
     }
 
     async fn receive_response(
-        receiver: oneshot::Receiver<Result<StateResponse, String>>,
-    ) -> Result<StateResponse, String> {
+        receiver: oneshot::Receiver<Result<StateResponse, StateClientError<StateRequest>>>,
+    ) -> Result<StateResponse, StateClientError<StateRequest>> {
         select! {
             _ = sleep(Duration::from_secs(5)) => {
-                Err(String::from("foo"))
+                Err(StateClientError::Timeout)
             },
             msg = receiver => {
                 match msg {
                     Ok(resp) => Ok(resp),
-                    Err(err) => Err(String::from("foo"))
+                    Err(err) => Err(err.into())
                 }
             }
-        }
-        .unwrap()
+        }?
     }
 }
 
@@ -100,11 +112,29 @@ impl ServerState {
     }
 
     async fn send_response(
-        resp: Result<StateResponse, String>,
-        sender: oneshot::Sender<Result<StateResponse, String>>,
+        resp: Result<StateResponse, StateClientError<StateRequest>>,
+        sender: oneshot::Sender<Result<StateResponse, StateClientError<StateRequest>>>,
     ) {
         if sender.send(resp).is_err() {
             error!("Error sending response to client. The receiver has been dropped");
         }
     }
+}
+
+#[derive(thiserror::Error, Debug)]
+pub enum StateClientError<T> {
+    #[error("")]
+    Send {
+        #[from]
+        source: tokio::sync::mpsc::error::SendError<T>,
+    },
+
+    #[error("")]
+    Recv {
+        #[from]
+        source: tokio::sync::oneshot::error::RecvError,
+    },
+
+    #[error("")]
+    Timeout,
 }
